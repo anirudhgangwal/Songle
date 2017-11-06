23c0b386f19ce49b81bbada2e27d505f44d71b5b
anirudhgangwal <s1504942@sms.ed.ac.uk>
2017-11-06 22:29:03 +0000

    Now downloading of songs.xml succesfull. Parsing of it to be implemented.

---
+++ app/src/main/java/com/cslp/anirudh/songle/DownloadXmlTask.kt
@@ -0,0 +1,72 @@
+package com.cslp.anirudh.songle
+
+import android.os.AsyncTask
+import org.xmlpull.v1.XmlPullParserException
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import android.util.Log
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+
+/**
+ * Created by anirudh on 06/11/17.
+ */
+
+class DownloadXmlTask(private val caller: DownloadCompleteListener) :
+        AsyncTask<String, Void, String>(){
+
+    val tag = "DownloadXmlTask"
+
+    override public fun doInBackground(vararg urls: String?): String {
+        return try {
+            Log.d(tag,"Do in background ... ")
+            loadXmlFromNetwork(urls[0])
+        } catch (e: IOException) {
+            "Unable to load content. Check your network connection"
+        } catch (e: XmlPullParserException) {
+            "Error parsing XML"
+        }
+    }
+
+    private fun loadXmlFromNetwork(urlString: String?): String {
+        Log.d(tag,"load xml from network ...")
+        val result = downloadUrl(urlString)
+        // Do something with stream e.g. parse as XML, build result
+        
+        Log.d(tag,"loadXmlFromNetwork returns "+ result)
+        return result
+    }
+
+    @Throws(IOException::class)
+    private fun downloadUrl(urlString: String?): String {
+        Log.d(tag,"download url ...")
+        val url = URL(urlString)
+        // Starts the query
+        val inp = url.openStream()
+        val reader = BufferedReader(InputStreamReader(inp))
+        val result = StringBuilder()
+        var line: String? = reader.readLine()
+        while (line != null) {
+            result.append(line)
+            line = reader.readLine()
+        }
+
+        Log.d(tag,"returning value\n " + result.toString())
+        return result.toString()
+    }
+
+    override fun onPostExecute(result: String?) {
+        super.onPostExecute(result)
+        if (result != null) {
+            Log.d(tag,"Post execution - calling download complete on caller")
+            caller.downloadComplete(result)
+        }
+    }
+}
+
+interface DownloadCompleteListener {
+    fun downloadComplete(result: String)
+}
--- app/src/main/java/com/cslp/anirudh/songle/MainActivity.kt
+++ app/src/main/java/com/cslp/anirudh/songle/MainActivity.kt
@@ -3,14 +3,25 @@ package com.cslp.anirudh.songle
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.webkit.DownloadListener
 
 class MainActivity : AppCompatActivity() {
 
+    val tag = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // download song list
+        val listener = SongDownloadListener()
+        val downloader = DownloadXmlTask(listener)
+        downloader.execute("http://www.inf.ed.ac.uk/teaching/courses/cslp/data/songs/songs.xml")
     }
 
     fun showListOfSongs(view: View):Unit {
@@ -20,3 +31,20 @@ class MainActivity : AppCompatActivity() {
 
 
 }
+
+
+class SongDownloadListener : DownloadCompleteListener{
+    override fun downloadComplete(result: String) {
+        Log.i("SongDownloadListener",result)
+        assert(result!=null)
+
+    }
+
+    fun parseXml(s: String):ArrayList<Song> {
+        var songs = ArrayList<Song>()
+
+        return songs
+    }
+}
+
+
